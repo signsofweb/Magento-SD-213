a2dc1a9697117c197fbc46864497bb20d377a36f
ChinhPv <chinhdays@gmail.com>
2018-06-30 16:37:12 +0700

    UPDATE CODE

--- app/code/Sow/Base/view/frontend/web/js/base.js
+++ app/code/Sow/Base/view/frontend/web/js/base.js
@@ -19,8 +19,14 @@ require([
             autoplaySpeed: 3000,
             autoplay: 1
         };
-        if ($(this).attr('data-slider') != undefined) {
-            options = JSON.parse($(this).attr('data-slider'));
+        var new_options = JSON.parse($(this).attr('data-slider'));
+        $.each(new_options,function(i,option) {
+            if(option == null){
+                new_options.i = options.i;
+            }
+        })
+        if (new_options != null) {
+             options = new_options;
         }
         $(this).owlCarousel({
             animateIn: 'fadeOutRight',
@@ -62,79 +68,6 @@ require([
 })
 /*
 ========== End Slider Owl  ==========
-*/
-/*
-==========  Popup Newsletter ==========
-*/
-require([
-    'jquery',
-    'magnificPopup'
-], function ($, modal) {
-    'use strict';
-    if ($(window).width() > 991) {
-        function setCookie(name, value, days) {
-            if (days) {
-                var date = new Date();
-                date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-                var expires = "; expires=" + date.toGMTString();
-            } else var expires = "";
-            document.cookie = name + "=" + value + expires + "; path=/";
-        }
-
-        function getCookie(name) {
-            var nameEQ = name + "=";
-            var ca = document.cookie.split(';');
-            for (var i = 0; i < ca.length; i++) {
-                var c = ca[i];
-                while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-            }
-            return null;
-        }
-
-        if (getCookie("show_popup_news") != 1) {
-            $.magnificPopup.open({
-                items: {
-                    src: '#popup_newsletter'
-                },
-                type: 'inline',
-                removalDelay: 300,
-                showCloseBtn: false,
-                callbacks: {
-                    beforeOpen: function () {
-                        this.st.mainClass = 'mfp-zoom-out modal_newsletter_popup';
-                    }
-                }
-            });
-            $('.popup_news_close').on('click', function () {
-                $.magnificPopup.close({
-                    items: {
-                        src: '#popup_newsletter'
-                    }
-                })
-            });
-
-        }
-        var cookie_time = $('#popup_newsletter').attr('data-cookie');
-        if (cookie_time == '') {
-            cookie_time = 1;
-        }
-        $("#newsletter_pop_up form").submit(function (event) {
-            setCookie("show_popup_news", '1', cookie_time);
-        });
-        $('#dont_show_popup_news_again').on('change', function () {
-            if (getCookie("show_popup_news") != cookie_time) {
-                setCookie("show_popup_news", '1', cookie_time)
-            } else {
-                setCookie("show_popup_news", '0', cookie_time)
-            }
-        });
-
-    }
-    ;
-});
-/*
-========== End Popup Newsletter ==========
 */
 /*
 ========== Scroll Top =====================
