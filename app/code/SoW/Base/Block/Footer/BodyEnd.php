eea6c7b70fa35edc580e7169c9606d787d8196ed
Chinh <chinhdays@gmail.com>
2018-07-26 00:40:35 +0700

    UPDATE

---
+++ app/code/SoW/Base/Block/Footer/BodyEnd.php
@@ -0,0 +1,17 @@
+<?php
+namespace SoW\Base\Block\Footer;
+class BodyEnd extends \Magento\Framework\View\Element\Template{
+    public $_coreRegistry;
+
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        \Magento\Framework\Registry $coreRegistry,
+        array $data = []
+    ) {
+        $this->_coreRegistry = $coreRegistry;
+        parent::__construct($context, $data);
+    }
+
+}
+
+?>
